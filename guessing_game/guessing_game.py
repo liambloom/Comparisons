7c4a9e18bbee2dce99fe7876f859bc44fc01012d
liambloom <liamrbloom@gmail.com>
2020-06-19 01:02:34 -0400

    Put body of some statements on same line
    This is the python equivalent of a blockless if statement

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -2,10 +2,8 @@ import random
 
 def shouldendgame():
   new_game = input("Do you want to play again? (y/n)\n")
-  if new_game == "y":
-    return False
-  elif new_game == "n":
-    return True
+  if new_game == "y": return False
+  elif new_game == "n": return True
   else:
     print('Invalid input. Please enter "y" or "n"')
     return shouldendgame()
@@ -23,21 +21,17 @@ for i in range(1, 10):
 
   while guess != secret_number:
     if (guess is not None): # == checks for equality (java .equals()), while is checks for being the same object (java ==)
-      if guess < secret_number:
-        print("Too large")
-      else:
-        print("Too small")
+      if guess < secret_number: print("Too large")
+      else: print("Too small")
     
     guess = input("Please input your guess.\n")
 
-    if guess.isdigit():
-      guess = int(guess)
+    if guess.isdigit(): guess = int(guess)
     else:
       print(str(guess) + " is not a number.")
       guess = None
   
   print("You win!")
-  if shouldendgame():
-    break
+  if shouldendgame(): break
 
 print("Thanks for playing")
